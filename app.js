37d737a186d4399870101531ff9b361a5c385d20
Dennys <dennys.ferrer@gmail.com>
2020-02-23 12:23:20 -0500

    Se incluyó en app.js la función getTasks

--- app.js
+++ app.js
@@ -8,9 +8,33 @@ function saveTask(e){
         description: description
     };
 
-    localStorage.setItem('task', JSON.stringify(task));
+    if (localStorage.getItem('tasks') === null){
+        let tasks = [];
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    } else {
+        let tasks = JSON.parse(localStorage.getItem('tasks'));
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
 
     e.preventDefault();
 }
 
+function getTasks(){
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    let tasksView = document.getElementById('tasks');
 
+    for (i=0; i < tasks.length; i++){
+        let title = tasks[i].title;
+        let description = tasks[i].description;
+        tasksView.innerHTML += "<div class="card">"
+            "<div class="card-body">"
+                "<p>${title} - ${description}</p>"
+                "<a class="btn btn-danger"></a>"
+            "</div>"
+        "</div>";
+    }
+}
+
+getTasks();
