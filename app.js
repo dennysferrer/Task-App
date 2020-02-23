33a25bdae3b7820e13014cac9c04d445a68a006d
Dennys <dennys.ferrer@gmail.com>
2020-02-22 21:38:15 -0500

    Se inicializó trabajo con el localStorage en archivo app.js

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 document.getElementById('formTask').addEventListener('submit', saveTask);
 
 function saveTask(e){
-    console.log(e);
-    alert('Enviando formulario');
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+    const task = {
+        title: title,
+        description: description
+    };
+
+    localStorage.setItem('task', JSON.stringify(task));
+
     e.preventDefault();
 }
+
+
